9297c02ee4a879bde1a7b69e3adba419c71b4382
rebootcomp <x.dovakin@yandex.ru>
2019-03-19 03:35:25 +0300

    gradle files and gitignore try

--- app/src/main/java/android/train/mipt_school/LoginActivity.java
+++ app/src/main/java/android/train/mipt_school/LoginActivity.java
@@ -1,11 +1,9 @@
 package android.train.mipt_school;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.design.button.MaterialButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
